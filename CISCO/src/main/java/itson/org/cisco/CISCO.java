56f3dbb227bdf75085d055910928a03fe573054d
michelle8q <cincastle584@gmail.com>
2026-06-10 09:14:55 -0700

    commit inicial

---
+++ CISCO/src/main/java/itson/org/cisco/CISCO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.org.cisco;
+
+/**
+ *
+ * @author cinca
+ */
+public class CISCO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
