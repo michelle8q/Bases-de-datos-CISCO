c5e22215150e2fe092cbd4b78ffc66c9a088f255
dannapina262718-tech <danna.pina262718@potros.itson.edu.mx>
2026-06-11 15:03:45 -0700

    frm bloqueo alumno

--- CISCO/src/main/java/presentacion/FrmBloqueoAlumno.java
+++ CISCO/src/main/java/presentacion/FrmBloqueoAlumno.java
@@ -27,31 +27,96 @@ public class FrmBloqueoAlumno extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        LblTituloTabla = new javax.swing.JLabel();
+        LblMotivo = new javax.swing.JLabel();
+        LblTitulo = new javax.swing.JLabel();
+        LblID = new javax.swing.JLabel();
+        TxtId = new javax.swing.JTextField();
+        TxtMotivo = new javax.swing.JTextField();
+        BtnCancelar = new javax.swing.JButton();
+        BtnBloquear = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 204));
 
-        LblTituloTabla.setFont(new java.awt.Font("Corbel", 1, 24)); // NOI18N
-        LblTituloTabla.setForeground(new java.awt.Color(0, 0, 0));
-        LblTituloTabla.setText("Bloquear alumno");
+        LblMotivo.setFont(new java.awt.Font("Corbel", 1, 24)); // NOI18N
+        LblMotivo.setText("Motivo:");
+
+        LblTitulo.setFont(new java.awt.Font("Corbel", 1, 24)); // NOI18N
+        LblTitulo.setText("Bloquear alumno");
+
+        LblID.setFont(new java.awt.Font("Corbel", 1, 24)); // NOI18N
+        LblID.setText("Id Alumno:");
+
+        TxtId.setText("id del alumno");
+
+        TxtMotivo.setText("motivo del bloqueo");
+
+        BtnCancelar.setBackground(new java.awt.Color(153, 0, 0));
+        BtnCancelar.setText("Cancelar");
+        BtnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCancelarActionPerformed(evt);
+            }
+        });
+
+        BtnBloquear.setBackground(new java.awt.Color(0, 102, 0));
+        BtnBloquear.setText("Boquear");
+        BtnBloquear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnBloquearActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(LblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(116, 116, 116))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(108, 108, 108)
-                .addComponent(LblTituloTabla, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(126, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(LblMotivo, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(TxtMotivo, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(LblID, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(TxtId, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(66, 66, 66)
+                        .addComponent(BtnCancelar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(BtnBloquear)))
+                .addContainerGap(57, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(LblTituloTabla)
-                .addContainerGap(354, Short.MAX_VALUE))
+                .addGap(36, 36, 36)
+                .addComponent(LblTitulo)
+                .addGap(40, 40, 40)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LblID)
+                    .addComponent(TxtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LblMotivo)
+                    .addComponent(TxtMotivo, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(BtnCancelar)
+                        .addGap(56, 56, 56))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(BtnBloquear)
+                        .addGap(47, 47, 47))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -67,6 +132,14 @@ public class FrmBloqueoAlumno extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCancelarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnCancelarActionPerformed
+
+    private void BtnBloquearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnBloquearActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnBloquearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -104,7 +177,13 @@ public class FrmBloqueoAlumno extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel LblTituloTabla;
+    private javax.swing.JButton BtnBloquear;
+    private javax.swing.JButton BtnCancelar;
+    private javax.swing.JLabel LblID;
+    private javax.swing.JLabel LblMotivo;
+    private javax.swing.JLabel LblTitulo;
+    private javax.swing.JTextField TxtId;
+    private javax.swing.JTextField TxtMotivo;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
