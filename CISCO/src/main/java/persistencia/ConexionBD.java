4e6b64119b2a96d3126ff10e3377439d421efe7c
michelle8q <cincastle584@gmail.com>
2026-06-11 22:29:40 -0700

    Conexion a la base y dependencia

---
+++ CISCO/src/main/java/persistencia/ConexionBD.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cinca
+ */
+public class ConexionBD implements IConexionBD {
+    final String SERVER = "localhost";
+    final String BASE_DATOS = "Cisco";
+    private final String CADENA_CONEXION = "jdbc:mysql://" + SERVER + "/" + BASE_DATOS;
+    final String USUARIO = "root";
+    final String CONTRASEÑA = "michelle";
+    
+    @Override
+    public Connection crearConexion() throws SQLException {
+        Connection conexion = DriverManager.getConnection(CADENA_CONEXION, USUARIO, CONTRASEÑA);
+        return conexion;   
+    }
+}
