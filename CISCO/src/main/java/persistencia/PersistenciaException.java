37f68fb2f9e072cb4ad09494d1ff6e8bf5454fd8
michelle8q <cincastle584@gmail.com>
2026-06-12 23:06:59 -0700

    Clases de excepciones por capas

---
+++ CISCO/src/main/java/persistencia/PersistenciaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistencia;
+
+/**
+ *
+ * @author cinca
+ */
+public class PersistenciaException extends Exception {
+    
+    public PersistenciaException(String mensaje) {
+        super(mensaje);
+    }
+}
